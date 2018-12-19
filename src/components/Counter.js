bf2b7f3e84c64cf3d954226bbc6e8f0663ab533c
GarryKim <moduedu@gmail.com>
2018-12-20 04:43:35 +0900

    Counter.js miss

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,12 +10,35 @@ import * as actions from '../actions';
 
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.setRandomColor = this.setRandomColor.bind(this);
+    }
+
+    setRandomColor() {
+        const color = [
+            Math.floor((Math.random()*55) + 200),
+            Math.floor((Math.random()*55) + 200),
+            Math.floor((Math.random()*55) + 200)
+        ];
+
+        this.props.handleSetColor(color);
+    }
     
     render() {
+
+        const color  = this.props.color;
+        const style = {
+            background:`rgb(${ color.join(',') })` //templete literals
+        };
         return (
-            <div>
+            <div style={style}>
                <Value number = {this.props.number}/>
-               <Control/>
+               <Control 
+                   onPlus={this.props.handleIncrement}
+                   onSubtract={this.props.handleDecrement}
+                   onRandomizeColor={this.setRandomColor}
+               />
             </div>
         )
     }
